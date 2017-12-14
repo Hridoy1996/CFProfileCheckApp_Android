fab9b342430eb5fc376606a61d1a069dcb1bf655
Hridoy <hridoytareqmahmud@gmail.com>
2017-12-14 11:52:26 +0600

    log in options clear

--- app/src/main/java/finalcf/magadistudio/com/finalcf/MainActivity.java
+++ app/src/main/java/finalcf/magadistudio/com/finalcf/MainActivity.java
@@ -6,9 +6,11 @@ import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -21,39 +23,45 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Objects;
 
 import static finalcf.magadistudio.com.finalcf.Main2Activity.*;
 
 public class MainActivity extends Activity  {
     public static String ss=null;
-    String isok="";
     EditText name;
+    int  chk  =0;
+    String profile =null;
 
     public  void login(View view)
     {
-        ss= name.getText().toString();
-        Intent intent =new Intent(MainActivity.this,Main2Activity.class);
 
-
-
-        if (ss == null || ss.isEmpty()) {
-            Toast.makeText(getApplicationContext(), " user name can't be empty " , Toast.LENGTH_LONG).show();
+       ss= name.getText().toString();
+        if( ss.isEmpty()) {
+            Toast.makeText(getApplication(), "khaili", Toast.LENGTH_LONG).show();
         }
-        else {
-            startActivity(intent);
+        else{
+            DownloadTask task = new DownloadTask();
+
+            task.execute("http://codeforces.com/api/user.info?handles="+ss);
         }
+
+
+
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         name=(EditText) findViewById(R.id.nn);
+
+
     }
-
-
     public class DownloadTask extends AsyncTask<String, Void, String> {
-        String image1=null;
+        String stattus=null;
 
         @Override
         protected String doInBackground(String... urls) {
@@ -86,66 +94,37 @@ public class MainActivity extends Activity  {
 
         @Override
         protected void onPostExecute(String res) {
+
             super.onPostExecute(res);
             try {
                 JSONObject jsonObject = new JSONObject(res);
-                String profile = jsonObject.getString("result");
-                JSONArray ara = new JSONArray(profile);
-                for (int i = 0; i < ara.length(); i++) {
+                 profile = jsonObject.getString("status");
+        if(!profile.isEmpty()) chk = 1 ;
+                else chk = 0;
 
-                    JSONObject elements = ara.getJSONObject(i);
 
-                   image1 = elements.getString("titlePhoto");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-                   // tv1.setText(  elements.getString("firstName") + " " +  elements.getString("lastName") );
+            Intent intent =new Intent(MainActivity.this,Main2Activity.class);
 
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
+            if(chk==1) {
+                startActivity(intent);
+                chk=0;
+            }
+                else{
+
+                Toast.makeText(getApplication()," wrong handle name ",Toast.LENGTH_LONG).show();
+
             }
 
-            ImageLoaderTask imageLoaderTask = new ImageLoaderTask();
-            imageLoaderTask.execute(image1);
+
+
         }
     }
 
-    private class ImageLoaderTask extends AsyncTask<String, Void, Drawable> {
-        @Override
-        protected Drawable doInBackground(String... imageUrls) {
-            Drawable image = null;
-            try {
-                String url = imageUrls[0];
-                if (null != url) {
-                    image = getDrawable(url);
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-                return null;
-            }
-            return image;
-        }
 
-        protected void onPostExecute(Drawable drawable) {
-            if (drawable != null) {
-               // ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                //imageView.setBackgroundDrawable(drawable);
-            }
-        }
-
-
-        private Drawable getDrawable(String address) {
-            try {
-                URL url = new URL(address);
-                InputStream is = (InputStream) url.getContent();
-                Drawable d = Drawable.createFromStream(is, "src");
-                return d;
-            } catch (MalformedURLException e) {
-                return null;
-            } catch (IOException e) {
-                return null;
-            }
-        }
-    }
 
 }
 
